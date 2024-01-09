ccf727f528bc6e208e18c521e92e247fab23bc1e
Smitty81-webs <82186230+Smitty81-webs@users.noreply.github.com>
2024-01-08 21:25:45 -0600

    johanning inc v2

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      animation: {
+        'shake': 'shake 0.82s cubic-bezier(.36,.07,.19,.97) both',
+        // Bounces 5 times 1s equals 5 seconds
+        'bounce': 'bounce 1s ease-in-out 10',
+      },
+      keyframes: {
+        'shake': {
+          '10%, 90%': {
+            transform: 'translate3d(-1px, 0, 0)'
+          },
+          '20%, 80%': {
+            transform: 'translate3d(2px, 0, 0)'
+          },
+          '30%, 50%, 70%': {
+            transform: 'translate3d(-4px, 0, 0)'
+          },
+          '40%, 60%': {
+            transform: 'translate3d(4px, 0, 0)'
+          }
+        },
+      },
+    },
+    plugins: [],
+  }
+}
